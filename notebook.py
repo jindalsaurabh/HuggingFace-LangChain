250bb441d455d8efbd854eb57554f09dd2c5e7a3
Saurabh Jindal <jindalsaurabh@gmail.com>
2024-10-31 10:46:49 +0100

    HuggingFace-LangChain

--- notebook.py
+++ notebook.py
@@ -1,6 +1,15 @@
 import os
 import langchain
-from langchain_community.llms import HuggingFaceHub
+#from langchain_community.llms import HuggingFaceHub
+#from langchain_huggingface import HuggingFaceHub
+from langchain_huggingface import HuggingFacePipeline
+from langchain_huggingface import ChatHuggingFace
+
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
+
+llm = HuggingFacePipeline(repo_id="tiiuae/falcon-7b-instruct")
+query = "There was a boy who went to school"
+output = llm.invoke(query)
+print(output)
